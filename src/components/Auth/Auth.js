a4dfbefb7d7db0e481b833c8f49d851b5518c5a1
nicol <nikolaicxxi@gmail.com>
2021-10-31 01:39:53 +0400

    add form submitting

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,53 +1,79 @@
 import React from 'react';
-import {Button, FormControl, IconButton, Input, InputAdornment, InputLabel, TextField} from "@material-ui/core";
+import {Button, FormControl, IconButton, Input, InputAdornment, InputLabel} from "@material-ui/core";
 import {Visibility, VisibilityOff} from "@material-ui/icons";
+import axios from "axios";
 
-const handleMouseDownPassword = (event) => {
-    event.preventDefault();
-};
-
-const Auth = function() {
+const Auth = () => {
     const [values, setValues] = React.useState({
-        password: '',
-        confirmedPassword: '',
+        password: "",
         showPassword: false,
+        email: "",
+        confirmedPassword: "",
+        showReg: false,
+        postId: null
     });
 
-    const handleChange = (prop) => (event) => {
-        setValues({ ...values, [prop]: event.target.value });
+    const handleClickShowReg = () => {
+        setValues({...values, showReg: !values.showReg});
     };
 
     const handleClickShowPassword = () => {
-        setValues({ ...values, showPassword: !values.showPassword });
+        setValues({...values, showPassword: !values.showPassword});
     };
-/*
-    const [showForAuth, setShowForAuth] = React.useState(true)
-    const [showForReg, setShowForReg] = React.useState(false)
-    const showReg = () => {
-        setShowForReg(true);
-        setShowForAuth(false);
-    }
-    const hideReg = () => {
-        setShowForReg(false);
-        setShowForAuth(true);
-    }*/
 
-    const ForAuth = function () {
-        return (
-            <div />
-        )
-    }
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
 
-    const ForReg = function () {
-        return (
-            <div />
-        );
+    const handleChange = (prop) => (event) => {
+        setValues({...values, [prop]: event.target.value});
+    };
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(values.showReg);
+        let json = null;
+        if (values.email !== '' && values.password !== '') {
+            if (values.showReg && values.password === values.confirmedPassword) {
+                json = JSON.stringify({
+                    firstName: 'Ivan',
+                    lastName: 'Ivanov',
+                    email: values.email,
+                    password: values.password
+                });
+
+                    /*'{"firstName": "Ivan", "lastName": "Ivanov", "email": "'
+                    + values.email
+                    + '", "password": "'
+                    + values.password
+                    + '"}';*/
+                axios.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
+                axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+                axios.post('http://localhost:8080/api/v1/registration', json)
+                    .then(response => setValues({...values, postId: response.data.id}));
+            } else if (!values.showReg) {
+                json = JSON.stringify({
+                    email: values.email,
+                    password: values.password
+                });
+                axios.defaults.headers.post['Content-Type'] ='application/x-www-form-urlencoded';
+                axios.post('http://localhost:8080/api/v1/registration', json)
+                    .then(response => setValues({...values, postId: response.data.id}));
+            }
+        }
+
     }
 
     return (
-        <div>
+        <form onSubmit={handleSubmit}>
             <div>
-                <TextField id="email" label="E-mail" type="email" />
+                <FormControl>
+                    <InputLabel htmlFor="email">E-mail</InputLabel>
+                    <Input
+                        id="email"
+                        value={values.email}
+                        onChange={handleChange('email')}/>
+                </FormControl>
             </div>
             <div>
                 <FormControl>
@@ -64,22 +90,173 @@ const Auth = function() {
                                     onClick={handleClickShowPassword}
                                     onMouseDown={handleMouseDownPassword}
                                 >
-                                    {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                                    {values.showPassword ? <Visibility/> : <VisibilityOff/>}
                                 </IconButton>
                             </InputAdornment>
                         }
                     />
                 </FormControl>
             </div>
-            <div>
-                <div className='forAuth'>
-                    <Button id="login" variant="outlined">Войти</Button>
-                </div>
-                <div className='forAuth'>
-                    <a id="toRegister" >Зарегистрироваться</a>
-                </div>
+            <div className='forAuth'>
+                <Button id="login" variant="outlined" type="submit">Войти</Button>
             </div>
-            <div>
+            <div className='forAuth'>
+                <a id="toRegister" onClick={handleClickShowReg}>Зарегистрироваться</a>
+            </div>
+            <div className='forReg'>
+                <FormControl>
+                    <InputLabel htmlFor="confirmedPassword">Повторите пароль</InputLabel>
+                    <Input
+                        id="confirmedPassword"
+                        type={values.showPassword ? 'text' : 'password'}
+                        value={values.confirmedPassword}
+                        onChange={handleChange('confirmedPassword')}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                >
+                                    {values.showPassword ? <Visibility/> : <VisibilityOff/>}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                    />
+                </FormControl>
+            </div>
+            <div className='forReg'>
+                <Button id="register" variant="outlined" type="submit">Зарегистрироваться</Button>
+            </div>
+            <div className='forReg'>
+                <a id="toAuth" onClick={handleClickShowReg}>Войти</a>
+            </div>
+        </form>
+    );
+};
+
+export default Auth;
+
+
+/*const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+};
+
+class Auth extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.email = React.createRef();
+        this.password = React.createRef();
+        this.confirmedPassword = React.createRef();
+        this.showReg = false;
+    }
+
+    handleSubmit(event) {
+        if (showReg) {
+            console.log(this.email);
+            console.log(this.password);
+            console.log(this.confirmedPassword);
+        } else {
+            console.log(this.email);
+            console.log(this.password);
+        }
+    }
+
+
+    const [values, setValues] = React   .useState({
+                 password: '',
+                 confirmedPassword: '',
+                 showPassword: false,
+             });
+
+    const
+    handleChange = (prop) => (event) => {
+        setValues({...values, [prop]: event.target.value});
+    };
+
+    const
+    handleClickShowPassword = () => {
+        setValues({...values, showPassword: !values.showPassword});
+    };
+    /!*
+        const [showForAuth, setShowForAuth] = React.useState(true)
+        const [showForReg, setShowForReg] = React.useState(false)
+        const showReg = () => {
+            setShowForReg(true);
+            setShowForAuth(false);
+        }
+        const hideReg = () => {
+            setShowForReg(false);
+            setShowForAuth(true);
+        }*!/
+
+    /!*const
+    ForAuth = function () {
+        return (
+            <div/>
+        )
+    }
+
+    const
+    ForReg = function () {
+        return (
+            <div/>
+        );
+    }*!/
+
+    function
+
+    formJson(e) {
+        e.preventDefault();
+        console.log('Отправлена форма');
+    }
+
+
+    /!* const formJson = function () {
+         const firstName = "Коля"
+         const lastName = "Саня"
+         const login = this.props.email
+         const password = this.props.password
+     }*!/
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div>
+                    <FormControl>
+                        <InputLabel htmlFor="email">E-mail</InputLabel>
+                        <Input id="email"/>
+                    </FormControl>
+                </div>
+                <div>
+                    <FormControl>
+                        <InputLabel htmlFor="password">Пароль</InputLabel>
+                        <Input
+                            id="password"
+                            type={values.showPassword ? 'text' : 'password'}
+                            value={values.password}
+                            onChange={handleChange('password')}
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleClickShowPassword}
+                                        onMouseDown={handleMouseDownPassword}
+                                    >
+                                        {values.showPassword ? <Visibility/> : <VisibilityOff/>}
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                        />
+                    </FormControl>
+                </div>
+                <div>
+                    <Button id="login" variant="outlined" onClick={formJson}>Войти</Button>
+                </div>
+                <div className='forAuth'>
+                    <a id="toRegister">Зарегистрироваться</a>
+                </div>
                 <div className='forReg'>
                     <FormControl>
                         <InputLabel htmlFor="confirmPassword">Повторите пароль</InputLabel>
@@ -95,7 +272,7 @@ const Auth = function() {
                                         onClick={handleClickShowPassword}
                                         onMouseDown={handleMouseDownPassword}
                                     >
-                                        {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                                        {values.showPassword ? <Visibility/> : <VisibilityOff/>}
                                     </IconButton>
                                 </InputAdornment>
                             }
@@ -106,15 +283,22 @@ const Auth = function() {
                     <Button id="register" variant="outlined">Зарегистрироваться</Button>
                 </div>
                 <div className='forReg'>
-                    <a id="toAuth" >Зарегистрироваться</a>
+                    <a id="toAuth">Войти</a>
                 </div>
-            </div>
-        </div>
-    );
+            </form>
+        );
+    }
 }
+/!*
+    Auth
+.
+    propTypes = {};
 
-Auth.propTypes = {};
+    Auth
+.
+    defaultProps = {};
 
-Auth.defaultProps = {};
+    export
+    default
+    Auth;*!/*/
 
-export default Auth;
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,30 @@
 import React from 'react';
 
+/*function Footer(props) {
+    return (
+        <footer>
+        <div className="footer__bottom-logos">
+            <a target="_blank" href="https://vk.com/startup__club"> <img className="footer-logo" src="/img/vk.svg"/></a>
+            <a target="_blank" href="https://www.instagram.com/startup__club/"> <img className="footer-logo"
+                                                                                     src="/img/instagram.svg"/></a>
+        </div>
+        <div className="footer__bottom-text">Exchance © {new Date().getFullYear()} |&nbsp;<a
+            href="mailto:exchance@ssau.ru">exchance@ssau.ru</a>
+        </div>
+    </footer>
+    );
+}*/
+
 
 const Footer = () => (
     <footer>
         <div className="footer__bottom-logos">
-            <a target="_blank" href="https://vk.com/startup__club"> <img className="footer-logo" src="/img/vk.svg" /></a>
-            <a target="_blank" href="https://www.instagram.com/startup__club/"> <img className="footer-logo" src="/img/instagram.svg" /></a>
+            <a target="_blank" href="https://vk.com/startup__club"> <img className="footer-logo" src="/img/vk.svg"/></a>
+            <a target="_blank" href="https://www.instagram.com/startup__club/"> <img className="footer-logo"
+                                                                                     src="/img/instagram.svg"/></a>
         </div>
-        <div className="footer__bottom-text">Exchance © {new Date().getFullYear()} |&nbsp;<a href="mailto:exchance@ssau.ru">exchance@ssau.ru</a>
+        <div className="footer__bottom-text">Exchance © {new Date().getFullYear()} |&nbsp;<a
+            href="mailto:exchance@ssau.ru">exchance@ssau.ru</a>
         </div>
     </footer>
 );
